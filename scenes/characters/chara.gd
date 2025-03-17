d495708e99541aaf6dbd47241de247b17cd31923
Robert Scoville <robertjamesscoves@gmail.com>
2025-03-17 14:59:29 -0500

    Work on player

--- scenes/characters/chara.gd
+++
@@ -1,8 +0,0 @@
-extends Node3D
-
-@onready var _anim_tree: AnimationTree = $AnimationTree
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
-	pass
