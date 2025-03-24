2e191d1157ceaf227401577052b88f6f8ca6eeb8
Robert Scoville <robertjamesscoves@gmail.com>
2025-03-24 11:00:14 -0500

    Shop Menu Added

--- scripts/characters/player.gd
+++ scripts/characters/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody3D
 
 @onready var player_mesh : Node3D = $chara
+@onready var anim_player : AnimationPlayer = $chara/AnimationPlayer
 @onready var spring_arm_pivot : Node3D = $SpringArmPivot
 @onready var ui_hp : TextureProgressBar = $HealthBar
 @onready var _anim_tree : AnimationTree = $chara/AnimationTree
@@ -35,6 +36,8 @@ var att = 1
 var def = 1
 var lvl = 1
 var heat = 0
+@export var money = 100
+@export var experience = 0
 
 # timer for stuff, as well as stun value and dodge count
 var timer = 0
@@ -43,6 +46,7 @@ var dge_count = 0
 
 var is_hurt = false
 @export var is_in_arena = true
+@export var at_shop = false
 var move_target : Vector3 = Vector3(0.0, 0.0, 0.0)
 
 func _ready():
@@ -51,8 +55,10 @@ func _ready():
 	heat_fx.emitting = true
 	heat_fx.speed_scale = 3
 	heat_fx.amount_ratio = 0
+	at_shop = false
 	
 func _physics_process(delta):
+	print(at_shop)
 	# display momentum amount
 	ui_heat.text = "Momentum: %s / 100" % [str(heat)]
 	
@@ -65,6 +71,13 @@ func _physics_process(delta):
 	
 	heat_fx.amount_ratio = heat * 0.01
 	
+	if Global.buying:
+		ui_hp.visible = false
+		ui_heat.visible = false
+	else:
+		ui_hp.visible = true
+		ui_heat.visible = true
+	
 	_anim_tree["parameters/playback"].travel("Idle")
 	dodge_fx.emitting = false
 	
@@ -75,138 +88,149 @@ func _physics_process(delta):
 	# tells you when you're attacking and what attack is being used
 	attacking = false
 	cur_attack = attacks[0]
-	
-	if health <= 0:
-		death()
-	
+	if Global.pause || Global.buying:
+		anim_player.speed_scale = 0
+		heat_fx.speed_scale = 0
+		dodge_fx.speed_scale = 0
+		kick_fx.speed_scale = 0
+		velocity.x = 0
+		velocity.z = 0
 	else:
-		if is_hurt:
-			if !Input.is_action_pressed("fight_stance"):
-				stun -= 1
-				_anim_tree["parameters/playback"].travel("Hit To Body")
-				if stun <= 0:
+		heat_fx.speed_scale = 2.8
+		dodge_fx.speed_scale = 1
+		kick_fx.speed_scale = 1
+		
+		if health <= 0:
+			death()
+		
+		else:
+			if is_hurt:
+				if !Input.is_action_pressed("fight_stance"):
+					stun -= 1
+					_anim_tree["parameters/playback"].travel("Hit To Body")
+					if stun <= 0:
+						is_hurt = false
+				else:
+					stun = 0
 					is_hurt = false
 			else:
-				stun = 0
-				is_hurt = false
-		else:
 
-			# Change speeds and makes sure you can't run when stanced
-			if Input.is_action_pressed("run"):
-				speed = RUN
-			elif Input.is_action_pressed("fight_stance") && !Input.is_action_pressed("run"):
-				_anim_tree["parameters/playback"].travel("Bouncing Fight Idle")	
-				speed = STANCE
-			else:
-				speed = WALK
-			if speed != STANCE:
-				# Inputs for the three attacks. Cannot do two attacks at once. Not elegant but it works
-				if Input.is_action_pressed("light_attack") && !Input.is_action_pressed("medium_attack") && !Input.is_action_pressed("heavy_attack"):
-					timer += 1
-					attacking = true
-					cur_attack = attacks[1]
-					if (timer % 33) == 17 || (timer % 33) == 18:
-						heat += 1
-						if heat > 100:
-							heat = 100
-						light_attack(att, heat)
-						
-				if Input.is_action_pressed("medium_attack") && !Input.is_action_pressed("heavy_attack"):
-					timer += 2
-					attacking = true
-					cur_attack = attacks[2]
-					if (timer % 62) == 34 || (timer % 62) == 35:
-						heat += 2
-						if heat > 100:
-							heat = 100
-						medium_attack(att, heat)
-						
-				if Input.is_action_pressed("heavy_attack"):
-					timer += 3
-					attacking = true
-					cur_attack = attacks[3]
-					if (timer % 77) == 30 || (timer % 77) == 31:
-						heat += 3					
-						if heat > 100:
-							heat = 100
-						heavy_attack(att, heat)
-					if (timer % 77) >= 30 && (timer % 77) <= 38:
-						kick_fx.emitting = true
-
-			
-			# set timer to 0 when attack buttons are pressed
-			if Input.is_action_just_released("heavy_attack"):
-				timer = 0
-			if Input.is_action_just_released("medium_attack"):
-				timer = 0
-			if Input.is_action_just_released("light_attack"):
-				timer = 0
-
-			
-			# get direction of movement
-			var direction : Vector3 = Vector3.ZERO
-			direction.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-			direction.z = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-			direction = direction.rotated(Vector3.UP, spring_arm_pivot.rotation.y)
-			
-			if Input.is_action_pressed("dodge"):
-				timer += 1
-				if timer % 60 >= 0 && timer % 60 <= 10:
-					if dge_count != 3:
-						if Input.is_action_pressed("fight_stance"):
-							dodge_fx.emitting = true
-							dge_count += .1
-						velocity.x = direction.x * DODGE
-						velocity.z = direction.z * DODGE
-					else:
-						if timer % 120 == 0:
-							dge_count = 0
+				# Change speeds and makes sure you can't run when stanced
+				if Input.is_action_pressed("run"):
+					speed = RUN
+				elif Input.is_action_pressed("fight_stance") && !Input.is_action_pressed("run"):
+					_anim_tree["parameters/playback"].travel("Bouncing Fight Idle")	
+					speed = STANCE
 				else:
-					velocity.x = move_toward(velocity.x, 0, speed)
-					velocity.z = move_toward(velocity.z, 0, speed)	
-				
-			# Add the gravity.
-			if not is_on_floor():
-				velocity += get_gravity() * delta
-
-			# Get the input direction and handle the movement/deceleration.
-			# As good practice, you should replace UI actions with custom gameplay actions.
-			var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-			
-			# A lot of what comes next could be a match statement, but this works for now (even if it is very ugly)
-			if direction:
-				if speed == WALK:
-					_anim_tree["parameters/playback"].travel("Walking")
-				elif speed == RUN:
-					_anim_tree["parameters/playback"].travel("Running")
-				elif speed == STANCE:
-					_anim_tree["parameters/playback"].travel("Bouncing Fight Idle")
+					speed = WALK
 				if speed != STANCE:
-					velocity.x = direction.x * speed
-					velocity.z = direction.z * speed
-			else:
-				if speed == STANCE:
-					_anim_tree["parameters/playback"].travel("Bouncing Fight Idle")
+					# Inputs for the three attacks. Cannot do two attacks at once. Not elegant but it works
+					if Input.is_action_pressed("light_attack") && !Input.is_action_pressed("medium_attack") && !Input.is_action_pressed("heavy_attack"):
+						timer += 1
+						attacking = true
+						cur_attack = attacks[1]
+						if (timer % 33) == 17 || (timer % 33) == 18:
+							heat += 1
+							if heat > 100:
+								heat = 100
+							light_attack(att, heat)
+							
+					if Input.is_action_pressed("medium_attack") && !Input.is_action_pressed("heavy_attack"):
+						timer += 2
+						attacking = true
+						cur_attack = attacks[2]
+						if (timer % 62) == 34 || (timer % 62) == 35:
+							heat += 2
+							if heat > 100:
+								heat = 100
+							medium_attack(att, heat)
+							
+					if Input.is_action_pressed("heavy_attack"):
+						timer += 3
+						attacking = true
+						cur_attack = attacks[3]
+						if (timer % 77) == 30 || (timer % 77) == 31:
+							heat += 3					
+							if heat > 100:
+								heat = 100
+							heavy_attack(att, heat)
+						if (timer % 77) >= 30 && (timer % 77) <= 38:
+							kick_fx.emitting = true
+
+				
+				# set timer to 0 when attack buttons are pressed
+				if Input.is_action_just_released("heavy_attack"):
+					timer = 0
+				if Input.is_action_just_released("medium_attack"):
+					timer = 0
+				if Input.is_action_just_released("light_attack"):
+					timer = 0
+
+				
+				# get direction of movement
+				var direction : Vector3 = Vector3.ZERO
+				direction.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+				direction.z = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+				direction = direction.rotated(Vector3.UP, spring_arm_pivot.rotation.y)
+				
+				if Input.is_action_pressed("dodge"):
+					timer += 1
+					if timer % 60 >= 0 && timer % 60 <= 10:
+						if dge_count != 3:
+							if Input.is_action_pressed("fight_stance"):
+								dodge_fx.emitting = true
+								dge_count += .1
+							velocity.x = direction.x * DODGE
+							velocity.z = direction.z * DODGE
+						else:
+							if timer % 120 == 0:
+								dge_count = 0
+					else:
+						velocity.x = move_toward(velocity.x, 0, speed)
+						velocity.z = move_toward(velocity.z, 0, speed)	
+					
+				# Add the gravity.
+				if not is_on_floor():
+					velocity += get_gravity() * delta
+
+				# Get the input direction and handle the movement/deceleration.
+				# As good practice, you should replace UI actions with custom gameplay actions.
+				var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+				
+				# A lot of what comes next could be a match statement, but this works for now (even if it is very ugly)
+				if direction:
+					if speed == WALK:
+						_anim_tree["parameters/playback"].travel("Walking")
+					elif speed == RUN:
+						_anim_tree["parameters/playback"].travel("Running")
+					elif speed == STANCE:
+						_anim_tree["parameters/playback"].travel("Bouncing Fight Idle")
+					if speed != STANCE:
+						velocity.x = direction.x * speed
+						velocity.z = direction.z * speed
 				else:
-					_anim_tree["parameters/playback"].travel("Idle")
-				velocity.x = move_toward(velocity.x, 0, speed)
-				velocity.z = move_toward(velocity.z, 0, speed)
-				
-			if attacking:	
-				if cur_attack == attacks[1]:
-					_anim_tree["parameters/playback"].travel("Punching")
-				elif cur_attack == attacks[2]:
-					_anim_tree["parameters/playback"].travel("Cross Punch")
-				elif cur_attack == attacks[3]:
-					_anim_tree["parameters/playback"].travel("Roundhouse Kick")	
-				velocity.x = move_toward(velocity.x, 0, speed)
-				velocity.z = move_toward(velocity.z, 0, speed)
+					if speed == STANCE:
+						_anim_tree["parameters/playback"].travel("Bouncing Fight Idle")
+					else:
+						_anim_tree["parameters/playback"].travel("Idle")
+					velocity.x = move_toward(velocity.x, 0, speed)
+					velocity.z = move_toward(velocity.z, 0, speed)
+					
+				if attacking:	
+					if cur_attack == attacks[1]:
+						_anim_tree["parameters/playback"].travel("Punching")
+					elif cur_attack == attacks[2]:
+						_anim_tree["parameters/playback"].travel("Cross Punch")
+					elif cur_attack == attacks[3]:
+						_anim_tree["parameters/playback"].travel("Roundhouse Kick")	
+					velocity.x = move_toward(velocity.x, 0, speed)
+					velocity.z = move_toward(velocity.z, 0, speed)
 
-			else:
-				if direction && !Input.is_action_pressed("fight_stance"):
-					player_mesh.rotation.y = lerp_angle(player_mesh.rotation.y, atan2(velocity.x, velocity.z), LERP_VALUE)
+				else:
+					if direction && !Input.is_action_pressed("fight_stance"):
+						player_mesh.rotation.y = lerp_angle(player_mesh.rotation.y, atan2(velocity.x, velocity.z), LERP_VALUE)
 
-				
+					
 	move_and_slide()
 			
 func light_attack(power, bonus):
--- scripts/environment/main.gd
+++ scripts/environment/main.gd
@@ -4,6 +4,7 @@ extends Node3D
 
 @onready var player : CharacterBody3D = $Player
 @onready var input_prompt : TextureRect = $Player/InputPrompt
+@onready var player_camera : Camera3D = $Player/SpringArmPivot/SpringArm3D/Camera3D
 @onready var spawner : Marker3D = $Spawner
 @onready var door_sensor : CollisionShape3D = $ShopDoor/CollisionShape3D
 @onready var door : MeshInstance3D = $PhysicalDoor
@@ -12,6 +13,12 @@ extends Node3D
 @onready var arena_area : Area3D = $Arena
 @onready var shopkeeper : AnimationTree = $Shopkeeper/AnimationTree
 @onready var shopkeeper_main : GPUParticles3D = $Shopkeeper/HeatParticles
+@onready var pause_text : Label = $PauseText
+@onready var shop_camera : Camera3D = $Shopkeeper/Camera3D
+@onready var shop_area : Area3D = $ShopArea
+@onready var shop_window : ColorRect = $ShopWindow
+@onready var shop_inventory : RichTextLabel = $ShopList
+@onready var shop_select : Label = $ShopSelect
 
 var spawn_time = 0
 
@@ -22,18 +29,48 @@ func _ready():
 	ui_text.visible = true
 	shopkeeper_main.emitting = false
 	input_prompt.visible = false
+	pause_text.visible = false
 	
 	
 func _process(delta):
+	if Input.is_action_just_pressed("start"):
+		if Global.pause:
+			Global.pause = false
+		elif !Global.pause && !Global.shop_time:
+			Global.pause = true
+	
+	if Global.pause:
+		pause_text.visible = true	
 	# enemy pathfinding
-	get_tree().call_group("enemies_g", "update_target_pos", player.global_transform.origin)
+	else:			
+		pause_text.visible = false
+		get_tree().call_group("enemies_g", "update_target_pos", player.global_transform.origin)
+		
+	if Global.buying:
+		shop_camera.current = true
+		ui_text.visible = false
+		input_prompt.visible = false
+		shop_window.visible = true
+		shop_inventory.visible = true
+		shop_select.visible = true
+		if Input.is_action_just_pressed("ui_down"):
+			shop_select.global_position.y += 23
+		if Input.is_action_just_pressed("ui_up"):
+			shop_select.global_position.y -= 23
+		
+	else:
+		player_camera.current = true
+		shop_window.visible = false
+		shop_inventory.visible = false
+		shop_select.visible = false
 	
 	# shop animation
 	shopkeeper["parameters/playback"].travel("Idle")
 	
 	# detects if player is inside the arena. If true, press start to begin the wave
 	if Global.shop_time == true:
-		ui_text.visible = true
+		if !Global.buying:
+			ui_text.visible = true
 		ui_text.text = "Go To Arena"
 		if player.is_in_arena:
 			ui_text.text = "Press Start to Begin"
@@ -42,17 +79,28 @@ func _process(delta):
 				Global.wave += 1
 		# shop door open during shop time
 		door.global_transform.origin.y = -25
+		if player.at_shop:
+			input_prompt.visible = true
+			if Input.is_action_just_pressed("interact"):
+				if !Global.buying:
+					Global.buying = true
+				else:
+					Global.buying = false
+		else:
+			input_prompt.visible = false
+		
 	else:
 		# shop door closed during fight time
 		door.global_transform.origin.y = 25
 		ui_text.visible = false
 		
 		# spawn a certain amount of enemies depending on the wave
-		if Global.enemies_spawned < Global.wave:
-			spawn_time += 1
-			if spawn_time % 60 == 2:
-				spawn(Global.wave)
-				Global.enemies_spawned += 1
+		if !Global.pause:
+			if Global.enemies_spawned < Global.wave:
+				spawn_time += 1
+				if spawn_time % 60 == 2:
+					spawn(Global.wave)
+					Global.enemies_spawned += 1
 
 # function that spawns the enemies
 func spawn(wave):
@@ -61,3 +109,8 @@ func spawn(wave):
 		enemy1.global_position = spawner.global_position
 		add_child(enemy1)
 		
+func _on_shop_area_area_entered(area: Area3D) -> void:
+	player.at_shop = true
+
+func _on_shop_area_area_exited(area: Area3D) -> void:
+	player.at_shop = false
